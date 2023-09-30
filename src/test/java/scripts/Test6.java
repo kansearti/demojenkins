5d973acab299379402f288881e7a6870126dbbbe
artik <artik@LAPTOP-SB3IPN86>
2023-09-30 22:46:01 +0530

    Pushed

---
+++ src/test/java/scripts/Test6.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test6 {
+
+	@Test
+	public void tc4() {
+		Reporter.log("Hello flinker", true);
+	}
+}
